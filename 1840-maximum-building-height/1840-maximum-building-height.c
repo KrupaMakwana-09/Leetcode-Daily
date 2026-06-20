5ab5f12e29843dede5b659924d942efed4e5cb93
Krupa makwana <krupamakwana16@gmail.com>
2026-06-20 13:46:53 +0530

    Time: 37 ms (100%), Space: 38.9 MB (83.33%) - LeetHub

---
+++ 1840-maximum-building-height/1840-maximum-building-height.c
@@ -0,0 +1,58 @@
+int cmp(const void* a, const void* b) {
+    int* x = *(int**)a;
+    int* y = *(int**)b;
+    return x[0] - y[0];
+}
+
+int maxBuilding(int n, int** restrictions, int restrictionsSize, int* restrictionsColSize) {
+    if (restrictionsSize == 0) {
+        return n - 1;
+    }
+
+    qsort(restrictions, restrictionsSize, sizeof(int*), cmp);
+
+    int idx = 1, h = 0;
+
+    for (int i = 0; i < restrictionsSize; i++) {
+        int x = restrictions[i][0];
+        int y = restrictions[i][1];
+
+        int limit = x - idx + h;
+        restrictions[i][1] = y < limit ? y : limit;
+
+        idx = x;
+        h = restrictions[i][1];
+    }
+
+    for (int i = restrictionsSize - 2; i >= 0; i--) {
+        int limit = restrictions[i + 1][1] + restrictions[i + 1][0] - restrictions[i][0];
+
+        if (limit < restrictions[i][1]) {
+            restrictions[i][1] = limit;
+        }
+    }
+
+    int res = n - restrictions[restrictionsSize - 1][0] + restrictions[restrictionsSize - 1][1];
+
+    idx = 1;
+    h = 0;
+
+    for (int i = 0; i < restrictionsSize; i++) {
+        int x = restrictions[i][0];
+        int y = restrictions[i][1];
+
+        int diff = y > h ? y - h : h - y;
+        int steps = x - idx - diff;
+        int higher = y > h ? y : h;
+
+        int candidate = higher + steps / 2;
+        if (candidate > res) {
+            res = candidate;
+        }
+
+        idx = x;
+        h = y;
+    }
+
+    return res;
+}
