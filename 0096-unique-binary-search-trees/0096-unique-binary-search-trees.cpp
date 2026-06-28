180dd5ae405a457391dc3a7fa5e1b5f5f5b01f5a
Krupa makwana <krupamakwana16@gmail.com>
2026-06-28 11:20:18 +0530

    Time: 0 ms (100%), Space: 7.8 MB (80.55%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int numTrees(int n) {
-        if(n <= 1) return 1;
-        int ans = 0;
-        for(int i = 1; i <= n; i++) 
-            ans += numTrees(i-1) * numTrees(n-i);
+        long long x = 1, ans;
+        for(int i = 1; i <= n; i++){
+            x = x *(i + n) / i;
+        }
+        ans = x / (n+1);
         return ans;
     }
 };
